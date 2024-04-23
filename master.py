a2c716fb093e3a47fcc59a77b65f654feeebbc76
nhelock <nhelock4003@stevenscollege.edu>
2024-04-23 12:22:33 -0400

    Functionality for accounts.html

--- master.py
+++ master.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import MySQLdb.cursors, re, hashlib
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
 
 app = Flask(__name__)
@@ -14,6 +17,11 @@ def login():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@app.route('/accounts')
+def accounts():
+    users = conn.execute(text(f"SELECT * FROM users")).all()
+    return render_template('accounts.html', users=users)
 
 
 if __name__ == '__main__':
