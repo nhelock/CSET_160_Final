d89736e1ad33718cab6b587dd42b9908d673f737
nhelock <nhelock4003@stevenscollege.edu>
2024-05-04 19:11:26 -0400

    Completed Assignment
    All Parameters for the assignment are completed.

--- master.py
+++ master.py
@@ -1,91 +1,8 @@
-# from flask import Flask, render_template, request
-# from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
-
-# app = Flask(__name__)
-# conn_str = "mysql://root:FunnyPassword321@localhost/160_final"
-# engine = create_engine(conn_str, echo=True)
-# conn = engine.connect()
-
-
-# @app.route('/')
-# def base():
-#     return render_template('login.html')
-
-
-# @app.route('/login')
-# def login():
-#     return render_template('login.html')
-
-
-# @app.route('/login', methods = [ "POST" ])
-# def login_route():
-#     return render_template('index.html')
-
-
-# @app.route('/signup')
-# def signup():
-#     return render_template('signup.html')
-
-
-# @app.route('/signup', methods = [ "POST" ])
-# def signup_button_route():
-#     return render_template('index.html')
-
-
-# @app.route('/signup', methods = [ "POST" ])
-# def signup_route():
-#     return render_template('index.html')
-
-
-# @app.route('/test_results')
-# def test_results():
-#     result = conn.execute(text("SELECT UserID, TestID, Grade FROM Grade")).fetchall()
-#     return render_template('test_results.html', grade=result)
-
-
-# @app.route('/test_responses')
-# def test_responses():
-#     students = conn.execute(text("SELECT CONCAT(FirstName, ' ', LastName) AS Name FROM users WHERE UserType = 'Student'")).fetchall()
-#     return render_template('test_responses.html', students=students)
-
-
-# @app.route('/test_responses', methods = [ "POST" ])
-# def test_responses_submit_route():
-#     result = conn.execute(text("SELECT UserID, TestID, Grade FROM Grade")).fetchall()
-#     return render_template('test_results.html', grade=result)
-
-
-# @app.route('/test_info')
-# def test_info():
-#     info = conn.execute(text("SELECT FirstName, LastName, UserType FROM users")).fetchall()
-#     return render_template('test_info.html', users=info)
-
-
-# @app.route('/test_details')
-# def test_details():
-#     details = conn.execute(text("SELECT FirstName, LastName, UserID, UserType FROM users where UserType = 'Student'")).fetchall()
-#     return render_template('test_details.html', users=details)
-
-
-# @app.route('/test_details', methods = [ "POST" ])
-# def test_details_student_route():
-#     students = conn.execute(text("SELECT CONCAT(FirstName, ' ', LastName) AS Name FROM users WHERE UserType = 'Student'")).fetchall()
-#     return render_template('test_responses.html', students=students)
-
-
-# @app.route('/test_list')
-# def test_list():
-#     students = conn.execute(text("SELECT CONCAT(FirstName, ' ', LastName) AS Name FROM users WHERE UserType = 'Student'")).fetchall()
-#     return render_template('test_list.html', students=students)
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import MySQLdb.cursors, re, hashlib
+import json
 
 app = Flask(__name__)
 
@@ -193,8 +110,8 @@ def register():
     return render_template('signup.html', msg=msg)
 
 
-@app.route('/test_results')
-def test_results():
+@app.route('/test_list')
+def test_list():
     # Check if user is logged in
     if 'loggedin' in session:
         # User is logged in, fetch test results from database
@@ -205,7 +122,7 @@ def test_results():
             JOIN users ON Tests.UserID = users.UserID
         ''')
         tests = cursor.fetchall()
-        return render_template('test_results.html', tests=tests)
+        return render_template('test_list.html', tests=tests)
     else:
         # User is not logged in, redirect to login page
         return redirect(url_for('login'))
@@ -213,15 +130,25 @@ def test_results():
 
 @app.route('/take_test/<test_id>')
 def take_test(test_id):
-    
+    if 'loggedin' not in session:
+        return redirect(url_for('login'))
+
+    # Search for Submissions
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM Grade WHERE TestID = %s AND UserID = %s", (test_id, session['id']))
+    submission = cursor.fetchone()
+
+    if submission:
+        return redirect(url_for('test_list'))
+
+    # Redirect to test:
     if session['UserType'] == 'Student':
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM questions WHERE TestID = %s', (test_id,))
         questions = cursor.fetchall()
-
         return render_template('take_test.html', test_id=test_id, questions=questions)
     else:
-        return redirect('/test_results')
+        return redirect('/test_list')
+
 
 
 @app.route('/take_test/<test_id>', methods=['POST'])
@@ -239,29 +166,80 @@ def submit_answers(test_id):
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
             for question_id, answer in answers.items():
                 cursor.execute("INSERT INTO responses (QuestionID, UserID, Answer) VALUES (%s, %s, %s)", (question_id, user_id, answer))
+            cursor.execute("INSERT INTO Grade (TestID, UserID) VALUES (%s, %s)", (test_id, user_id))
             mysql.connection.commit()
-            return redirect(url_for('test_results'))
+            return redirect(url_for('test_list'))
         except Exception as e:
             return str(e)
     else:
         return "Method not allowed"
+    
+
+
+@app.route('/test_grades/<test_id>')
+def test_grades(test_id):
+    try:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("""
+            SELECT r.UserID, q.Question, r.Answer, g.Grade
+            FROM responses r
+            INNER JOIN questions q ON r.QuestionID = q.QuestionID
+            LEFT JOIN Grade g ON r.UserID = g.UserID AND q.TestID = g.TestID
+            WHERE q.TestID = %s
+        """, (test_id,))
+        submissions = cursor.fetchall()
+
+        # Group submissions by user
+        grouped_submissions = {}
+        for submission in submissions:
+            user_id = submission['UserID']
+            if user_id not in grouped_submissions:
+                grouped_submissions[user_id] = []
+            grouped_submissions[user_id].append(submission)
+
+        return render_template('test_grades.html', test_submissions=grouped_submissions, test_id=test_id)
+    except Exception as e:
+        return str(e)
+
+    
+
+@app.route('/test_grades/<test_id>/', methods=['POST'])
+def grade_submission(test_id):
+    if 'loggedin' not in session:
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        try:
+
+            grade = request.form.get('grade')
+            
+            student_id = request.form.get('student_id')
+
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("UPDATE Grade SET Grade = %s WHERE UserID = %s AND TestID = %s", (grade, student_id, test_id))
+            mysql.connection.commit()
+
+            return redirect(url_for('test_grades', test_id=test_id))
+        except Exception as e:
+            return str(e)
+    else:
+        return redirect(url_for('test_grades', test_id=test_id))
+
+
+
 
 
 
 @app.route('/accounts')
 def accounts():
     try:
-        # Establish database connection
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         
-        # Fetch all user accounts from the database
         cursor.execute("SELECT UserID, FirstName, LastName, UserType FROM users ORDER BY UserType")
         users = cursor.fetchall()
         
-        # Close database connection
         cursor.close()
         
-        # Render the accounts.html template with the user data
         return render_template('accounts.html', users=users)
     except Exception as e:
         return str(e)
@@ -278,6 +256,135 @@ def show_tests():
     ''')
     tests = cursor.fetchall()
     return render_template('tests.html', tests=tests)
+
+
+
+@app.route('/delete_test/<test_id>', methods=['POST'])
+def delete_test(test_id):
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("DELETE FROM Tests WHERE TestID = %s", (test_id,))
+        mysql.connection.commit()
+        return redirect(url_for('show_tests'))
+    except Exception as e:
+        return str(e)
+
+
+
+@app.route('/edit_test/<test_id>', methods=['GET'])
+def show_test(test_id):
+    if request.method == 'GET':
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM Tests WHERE TestID = %s", (test_id,))
+        test_details = cursor.fetchone()
+
+        cursor.execute("SELECT QuestionID, Question FROM Questions WHERE TestID = %s", (test_id,))
+        questions = cursor.fetchall()
+
+        return render_template('edit_test.html', test_id=test_id, test_details=test_details, questions=questions)
+
+
+from flask import request
+
+@app.route('/edit_test/<test_id>', methods=['POST'])
+def edit_test(test_id):
+    try:
+        
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        
+        for question_id, question_text in request.form.items():
+            if question_id.startswith('question_'):
+                question_id = question_id.split('_')[1]
+                delete_checkbox = request.form.get('delete_' + question_id)
+                if delete_checkbox:
+
+                    cursor.execute("DELETE FROM Questions WHERE QuestionID = %s", (question_id,))
+                else:
+
+                    cursor.execute("UPDATE Questions SET Question = %s WHERE QuestionID = %s", (question_text, question_id))
+        
+        mysql.connection.commit()
+        
+        return redirect(('/tests'))
+    except Exception as e:
+        return str(e)
+    
+
+
+
+
+
+@app.route('/create_test', methods=['GET'])
+def create_test():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    try:
+
+        cursor.execute("INSERT INTO Tests (UserID) VALUES (%s)", (session['id'],))
+        mysql.connection.commit()
+        
+        cursor.execute("SELECT LAST_INSERT_ID() as TestID")
+        testid = cursor.fetchone()['TestID']
+        
+        return redirect(url_for('add_questions', test_id=testid))
+    except Exception as e:
+        return str(e)
+
+
+
+@app.route('/create_test', methods=['POST'])
+def create_questions():
+    try:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT LAST_INSERT_ID() as TestID")
+        test_id = cursor.fetchone()['TestID']
+
+        return redirect(url_for('add_questions/<test_id>', test_id=test_id))
+    except Exception as e:
+        return str(e)
+
+
+
+
+@app.route('/add_questions/<test_id>', methods=['GET', 'POST'])
+def add_questions(test_id):
+    if request.method == 'POST':
+        try:
+            for key, value in request.form.items():
+                if key.startswith('question_'):
+                    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                    cursor.execute("INSERT INTO Questions (TestID, Question) VALUES (%s, %s)", (test_id, value))
+                    mysql.connection.commit()
+
+            return redirect('/tests')
+        except Exception as e:
+            return str(e)
+    else:
+        return render_template('add_questions.html', test_id=test_id)
+
+
+
+
+
+
+
+@app.route('/my_grade')
+def grades():
+    if 'loggedin' not in session:
+        return redirect(url_for('login'))
+    
+    try:
+        user_id = session['id']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT TestID, Grade FROM Grade WHERE UserID = %s", (user_id,))
+        grades = cursor.fetchall()
+        
+
+        return render_template('my_grade.html', grades=grades)
+    except Exception as e:
+        return str(e)
+
 
 
 
